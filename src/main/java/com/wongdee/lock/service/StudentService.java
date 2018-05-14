ad5309b085c14671790feff43c66f5b850ac1b7f
wangdiz <wangdiz@seentao.com>
2018-05-14 17:00:38 +0800

    Signed-off-by: wangdiz <wangdiz@seentao.com>

--- src/main/java/com/wongdee/lock/service/StudentService.java
+++ src/main/java/com/wongdee/lock/service/StudentService.java
@@ -1,9 +1,11 @@
 package com.wongdee.lock.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.wongdee.lock.domain.StudentMapper;
 
+@Service
 public class StudentService {
 	
 	@Autowired
